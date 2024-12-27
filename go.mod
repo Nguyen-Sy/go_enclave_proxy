c4eb451c104d12aef3ce7b4dcbedffd32832b319
Nguyen-Sy <synguyentien.work@gmail.com>
2024-12-27 14:52:40 +0700

    chore: update v3

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main
 go 1.23.4
 
 require (
+	github.com/linuxkit/virtsock v0.0.0-20241009230534-cb6a20cc0422 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
 	github.com/mdlayher/vsock v1.2.1 // indirect
 	golang.org/x/net v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,148 +1,51 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io"
 	"log"
-	"os"
-	"strings"
+	"time"
 
-	"github.com/mdlayher/vsock"
+	"github.com/linuxkit/virtsock/pkg/vsock"
 )
 
-const (
-	BUFF_SIZE = 4096
-	PORT      = 5000
-	CID       = 16
-)
-
-// readAll reads from the connection until EOF or error
-func readAll(conn *vsock.Conn) (string, error) {
-	var result strings.Builder
-	buffer := make([]byte, BUFF_SIZE)
+func main() {
+	const (
+		cid  = 3    // Replace with the Nitro Enclave CID
+		port = 5000 // Port to connect to
+	)
 
 	for {
-		n, err := conn.Read(buffer)
+		conn, err := vsock.Dial(cid, port)
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return "", fmt.Errorf("read error: %v", err)
-		}
-		result.Write(buffer[:n])
-		if n < BUFF_SIZE {
-			break
-		}
-	}
-
-	return result.String(), nil
-}
-
-// server starts a vsock server
-func server(port uint32) error {
-	// Create vsock listener
-	listener, err := vsock.Listen(port, &vsock.Config{})
-	if err != nil {
-		return fmt.Errorf("failed to create listener: %v", err)
-	}
-	defer listener.Close()
-
-	log.Printf("Started server on port %d", port)
-
-	for {
-		// Accept connection
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Accept failed: %v", err)
+			log.Printf("Failed to connect to vsock server: %v", err)
+			time.Sleep(5 * time.Second)
 			continue
 		}
 
-		// Handle each connection in a goroutine
-		go func(c *vsock.Conn) {
-			defer c.Close()
+		log.Println("Connected to vsock server")
 
-			addr := c.RemoteAddr().String()
-			log.Printf("New connection to addr: %s", addr)
-
-			// Read message
-			msg, err := readAll(c)
-			if err != nil {
-				log.Printf("Failed to read message: %v", err)
-				return
-			}
-
-			log.Printf("Received: %s", msg)
-
-			// Echo message back
-			_, err = c.Write([]byte(msg))
-			if err != nil {
-				log.Printf("Failed to send response: %v", err)
-				return
-			}
-		}(conn.(*vsock.Conn))
-	}
-}
-
-// client connects to a vsock server and sends messages
-func client(cid, port uint32) error {
-	// Connect to the server
-	conn, err := vsock.Dial(cid, port, &vsock.Config{})
-	if err != nil {
-		return fmt.Errorf("connection failed: %v", err)
-	}
-	defer conn.Close()
-
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		// Read input from user
-		fmt.Print("Enter message (or 'quit' to exit): ")
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			return fmt.Errorf("failed to read input: %v", err)
-		}
-
-		// Trim whitespace and check for quit command
-		message = strings.TrimSpace(message)
-		if message == "quit" {
-			return nil
-		}
-
-		// Send message
+		message := "hello"
 		_, err = conn.Write([]byte(message))
 		if err != nil {
-			return fmt.Errorf("failed to send message: %v", err)
+			log.Printf("Failed to send message: %v", err)
+			conn.Close()
+			time.Sleep(5 * time.Second)
+			continue
 		}
 
-		// Read response
-		response, err := readAll(conn)
+		log.Printf("Sent message: %s", message)
+
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
 		if err != nil {
-			return fmt.Errorf("failed to read response: %v", err)
+			log.Printf("Failed to read response: %v", err)
+			conn.Close()
+			time.Sleep(5 * time.Second)
+			continue
 		}
 
-		fmt.Printf("Response from server: %s\n", response)
-	}
-}
+		log.Printf("Received response: %s", string(buffer[:n]))
+		conn.Close()
 
-func main() {
-	// Parse command line arguments
-	isServer := flag.Bool("server", false, "Run as server")
-	port := flag.Uint("port", PORT, "Port to listen on/connect to")
-	cid := flag.Uint("cid", CID, "CID to connect to (client only)")
-	flag.Parse()
-
-	var err error
-	if *isServer {
-		fmt.Println("Starting server...")
-		err = server(uint32(*port))
-	} else {
-		fmt.Printf("Starting client, connecting to CID: %d, Port: %d...\n", *cid, *port)
-		err = client(uint32(*cid), uint32(*port))
-	}
-
-	if err != nil {
-		log.Fatalf("Error: %v", err)
+		time.Sleep(5 * time.Second)
 	}
 }
